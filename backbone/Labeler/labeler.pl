766a94cd3e29776abff4b4d2a4bec27f88986a10
David Crabb <ramball47@yahoo.com>
2011-07-13 14:09:43 -0400

    Made it so labeler can be run multiple times on a file. Setting up for some big changes in the PANGEA backbone. Not Tested.

--- backbone/Labeler/labeler.pl
+++ backbone/Labeler/labeler.pl
@@ -4,8 +4,8 @@
 # Written by: David B. Crabb
 # Eric Triplett's Group
 # University of Florida
-# Last Modified: July 8, 2010
-#################################################
+# Last Modified: July 13, 2011
+#####################################################
 #
 #	Parameters:
 #		-s sequences input
@@ -15,8 +15,10 @@
 #		-e e-value upper threshold (DEFAULT: -20)
 #		-b bitscore lower threshold (200)
 #		-d directory of output
+# 		-o base output name
+# 		-l number of times input analyzed by labeler
 #
-#################################################
+#####################################################
 
 use Getopt::Std;
 
@@ -26,40 +28,51 @@ $ETHRESHOLD = exp(-20);								# Default e-value upper threshold
 $BTHRESHOLD = 200;									# Default bitscore lower threshold
 $dir = "";											# Default output directory is the current directory
 
-$printLoc = -1;
-getopts('s:m:p:d:', \%parameters);					# Takes parameters
+$printLoc = -1;										# Will tell the script which file to print a sequence in. Unclassified = 0, Classified = 1
+getopts('s:m:p:o:d:e:b:l:', \%parameters);			# Takes parameters
 
 unless($parameters{s} && $parameters{m} && $parameters{p})
 {
 	print "Must enter the -s sequences file -m megablast result file and -p similarity level.\n";
 	exit;
-}
+}	
 
-$extIndex = index($parameters{s}, ".") + 1;			# Determines the location of the "." of the extension of a file or returns 0 if it is not there
+$extIndex = index($parameters{s}, ".");				# Determines the location of the "." of the extension of a file or returns -1 if it is not there
 if($extIndex > 0)
 {
-	$out = substr($parameters{s}, 0, $extIndex);	# If there is an extension, take everything up until, but not including extension
+    $out = substr($parameters{s}, 0, $extIndex);	# If there is an extension, take everything up until, but not including extension
 }
 else
 {
-	$out = $parameters{s};							# If there is no extension, no need to adjust
-}	
+    $out = $parameters{s};							# If there is no extension, no need to adjust
+}
+
+if($parameters{o})
+{
+	$out = $parameters{o};							# Set the base output name as defined by the user
+}
 
 $PTHRESHOLD = $parameters{p};						# Set the similarity level as defined by the user
 
 if($parameters{e})									# If user defined a new e-value upper threshold
 {
-	$ethres = exp($parameters{e});
+	$ETHRESHOLD = exp($parameters{e});
 }
 
 if($parameters{b})									# If user defined a new bitscore lower threshold
 {
-	$bthres = $parameters{b};
+	$BTHRESHOLD = $parameters{b};
 }
 
 if($parameters{d})									# If user defined a new output directory
 {
 	$dir = "$parameters{d}";
+}
+
+$num_labeled = -1;
+if($parameters{l})
+{
+	$num_labeled = $parameters{l};
 }
 
 unless (open(SEQIN, $parameters{s}))       			# Try to open file
@@ -73,25 +86,27 @@ unless (open(MEGAIN, $parameters{m}))       		# Try to open file
 	exit;
 }
 
-open CLASSOUT, ">$dir"."C_$out"."_$pthres.fas" or die $!;
-open UNCLASSOUT, ">$dir"."U_$out"."_$pthres.fas" or die $!;
+open CLASSOUT, ">$dir"."C$PTHRESHOLD"."_$out.fas" or die $!;
+open UNCLASSOUT, ">$dir"."U$PTHRESHOLD"."_$out.fas" or die $!;
 $megaLine = <MEGAIN>;
 while($seqLine = <SEQIN>)
 {
 	if($seqLine =~ />/)								# If new sequence...
 	{
-		@seqHeading = split("_", $seqLine);
-		$size = scalar @seqHeading;
-		$seqHeading[0] = $seqHeading[0]."_";					#adds original underscore since it just split
-		$seqHeading[$size - 1] = "_".$seqHeading[$size -1];		#adds extra underscore
+		$seqLine =~ s/\s+$//;						# Removes white space
+		$seqName = substr($seqLine, 1);				# Removes ">" character
 		
-		$seqDex = index($seqLine, "\n") - 2;
-		$seqName = substr($seqLine, 1, $seqDex);
-
-		@megaName = split("\t", $megaLine);						#split the megablast output file up
+		if($num_labeled > 0)
+		{
+			$start = $num_labeled * 4;				# Will skip the labels
+			$seqName = substr($seqLine, $start);
+		}
+		
+		@megaName = split("\t", $megaLine);			# Split the megablast output file up
 		if($seqName eq $megaName[0])
 		{
-			if($megaName[2] < $pthres || $megaName[10] > $ethres || $megaName[11] < $bthres)
+			$seqName = substr($seqLine, 1);			# Restores the labels for further labeling
+			if($megaName[2] < $PTHRESHOLD || $megaName[10] > $ETHRESHOLD || $megaName[11] < $BTHRESHOLD)
 			{
 				printU();
 				$printLoc = 1;
@@ -134,18 +149,10 @@ close UNCLASSOUT;
 #######################################	  SUBROUTINES	#######################################
 sub printU()
 {
-	print UNCLASSOUT $seqHeading[0]."U";
-	for($a = 1; $a < $size; $a++)
-	{
-		print UNCLASSOUT "$seqHeading[$a]";
-	}
+	print UNCLASSOUT ">U$PTHRESHOLD"."_$seqName\n";
 }
 
 sub printC()
 {
-	print CLASSOUT $seqHeading[0]."C";
-	for($a = 1; $a < $size; $a++)
-	{
-		print CLASSOUT "$seqHeading[$a]";
-	}
+	print CLASSOUT ">C$PTHRESHOLD"."_$seqName\n";
 }
